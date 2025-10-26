ff8ce42543a580900cd97d6c6b95c6530476e0e8
nick-plumb <nhplumb27@wabash.edu>
2025-10-26 12:41:21 -0400

    CSC271 10/23

--- include/mesh.h
+++ include/mesh.h
@@ -12,6 +12,9 @@
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
+// #include <assimp/Importer.hpp>
+// #include <assimp/scene.h>
+// #include <assimp/postprocess.h>
 // #include"csrddef"
 
 struct VertexAttribute {
